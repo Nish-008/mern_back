f0e09d26dfb8c2a43075f28b4c63f0a3271b5ac8
Nish-008 <farihazaman.nishat@gmail.com>
2024-01-29 12:28:11 +0600

    trying to fetch data

--- db.js
+++ db.js
@@ -1,18 +1,44 @@
+// db.js
 const mongoose = require('mongoose');
 const mongoURI = 'mongodb+srv://farihazamannishat:nishat1234@cluster0.tcxcl1g.mongodb.net/MonerMotonDB?retryWrites=true&w=majority';
 
-const mongoDB = () => {
-    mongoose.connect(mongoURI);
+const connectToMongoDB = async () => {
+    try {
+        const startTime = new Date();
 
-    const db = mongoose.connection;
+        await mongoose.connect(mongoURI, {});
 
-    db.on('error', (err) => {
-        console.error('MongoDB connection error:', err);
-    });
+        const endTime = new Date();
+        console.log('Time taken to establish connection:', endTime - startTime, 'ms');
 
-    db.once('open', () => {
-        console.log('Connected to MongoDB');
-    });
-}
+        const db = mongoose.connection;
 
-module.exports = mongoDB;
+        db.on('error', (err) => {
+            console.error('MongoDB connection error:', err);
+        });
+
+        db.once('open', () => {
+            console.log('Connected to MongoDB');
+
+            const fetchDataStartTime = new Date();
+            const fetched_data = mongoose.connection.db.collection("food_items");
+
+            fetched_data.find({}).toArray((err, data) => {
+                const fetchDataEndTime = new Date();
+                console.log('Time taken to fetch data:', fetchDataEndTime - fetchDataStartTime, 'ms');
+                
+                if (err) {
+                    console.error(err);
+                } else {
+                    console.log(data);
+                }
+            });
+        });
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error);
+    }
+};
+
+module.exports = connectToMongoDB;
+
+
